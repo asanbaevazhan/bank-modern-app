4e1a37db42b7934bf01ba4492e52caa468bef52f
Aizhan <asanbaevazhan@gmail.com>
2022-12-13 00:16:44 +0600

    latest

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../components'
+
+const GetStarted = () =>  (
+    <div className={`${styles.flexCenter}`}>
+      GetStarted
+    </div>
+  )
+
+
+export default GetStarted
